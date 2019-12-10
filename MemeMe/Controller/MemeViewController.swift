db54bb6c4dc0e3662262087ccd0576e25b535957
Wladmir Júnior <wladmir.silva@bancointer.com.br>
2019-12-10 02:07:46 -0300

    Up project

---
+++ MemeMe/Controller/MemeViewController.swift
@@ -0,0 +1,200 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Wladmir Edmar Silva Junior on 09/12/19.
+//  Copyright © 2019 Wladmir Edmar Silva Junior. All rights reserved.
+//
+
+import UIKit
+
+struct Constants {
+    static let topText = "TOP"
+    static let bottomText = "BOTTOM"
+    static let saveSuccessTitle = "Saved!"
+    static let saveSuccessMessage = "Your image has been saved to your photos."
+    static let saveErrorTitle = "Save error"
+    static let ok = "OK"
+}
+
+class MemeViewController: UIViewController {
+
+    // MARK: - PROPERTIES
+    
+    @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var toolbar: UIToolbar!
+    
+    // MARK: - LIFECYCLE
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        shareButton.isEnabled = false
+        
+        let memeTextAttributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.strokeColor: UIColor.black,
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSAttributedString.Key.strokeWidth:  -4.0
+        ]
+        
+        topTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        
+        topTextField.text = Constants.topText
+        topTextField.delegate = self
+        topTextField.textAlignment = .center
+        
+        bottomTextField.text = Constants.bottomText
+        bottomTextField.delegate = self
+        bottomTextField.textAlignment = .center
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unsubscribeToKeyboardNotifications()
+    }
+
+    // MARK: - ACTIONS
+    
+    @IBAction func pickAnImageFromAlbum(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func pickAnImageFromCamera(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .camera
+        present(pickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func shareAction(_ sender: Any) {
+        let memedImage = generateMemedImage()
+        let meme = Meme(topText: topTextField.text!, bottomText: bottomTextField.text!, originalImage: imagePickerView.image!, memeImage: memedImage)
+        
+        let activityController = UIActivityViewController(activityItems: [memedImage], applicationActivities: nil)
+        present(activityController, animated: true, completion: nil)
+        
+        activityController.completionWithItemsHandler = { [weak self] activity, success, items, error in
+            if success {
+                self?.dismiss(animated: true, completion: nil)
+                self?.save(meme)
+            }
+        }
+    }
+    
+    @IBAction func cancelAction(_ sender: Any) {
+        topTextField.text = Constants.topText
+        bottomTextField.text = Constants.bottomText
+        imagePickerView.image = nil
+    }
+    
+    // MARK: - PRIVATE API
+    
+    func save(_ meme: Meme) {
+        UIImageWriteToSavedPhotosAlbum(meme.memeImage, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {            
+            showAlertWith(title: Constants.saveErrorTitle, message: error.localizedDescription)
+        } else {
+            showAlertWith(title: Constants.saveSuccessTitle, message: Constants.saveSuccessMessage)
+        }
+    }
+    
+    func showAlertWith(title: String, message: String){
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: Constants.ok, style: .default))
+        present(ac, animated: true)
+    }
+
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIWindow.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIWindow.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeToKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIWindow.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIWindow.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        if bottomTextField.isFirstResponder {
+            view.frame.origin.y = -getKeyboardHeight(notification)
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        if bottomTextField.isFirstResponder {
+            view.frame.origin.y = 0;
+        }
+    }
+    
+    func getKeyboardHeight(_ notification: Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    
+    func generateMemedImage() -> UIImage {
+        hideItems(true)
+        
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+
+        hideItems(false)
+        return memedImage
+    }
+    
+    func hideItems(_ hidden: Bool) {
+        toolbar.isHidden = hidden
+        navigationController?.navigationBar.isHidden = hidden
+    }
+}
+
+extension MemeViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imagePickerView.image = image
+            
+            DispatchQueue.main.async {
+                self.shareButton.isEnabled = true
+            }
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+extension MemeViewController: UITextFieldDelegate {
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == Constants.topText || textField.text == Constants.bottomText {
+            textField.text = ""
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
---
+++ MemeMe/Model/Meme.swift
@@ -0,0 +1,17 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Wladmir Edmar Silva Junior on 10/12/19.
+//  Copyright © 2019 Wladmir Edmar Silva Junior. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memeImage: UIImage
+}
